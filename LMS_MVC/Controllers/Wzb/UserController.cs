0532d1d1e49bcdf735dfc531952fe9e16b6a3cd9
王则波 <@DESKTOP-HEGK1L8>
2020-09-18 15:26:57 +0800

    wzb2

---
+++ LMS_MVC/Controllers/Wzb/UserController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMS_MVC.Controllers.Wzb
+{
+    public class UserController : Controller
+    {
+        public IActionResult KeIndex()
+        {
+            return View();
+        }
+    }
+}
