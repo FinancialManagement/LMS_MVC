dac24d3fd67ad93a179f43a19da6c038b488d9a1
Administrator <Administrator@WIN-9DHK002JTM1>
2020-09-25 19:21:07 +0800

    djq

---
+++ LMS_MVC/Controllers/ADFinanl/ADController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMS_MVC.Controllers.ADFinanl
+{
+    public class ADController : Controller
+    {
+        public IActionResult FangMoeny()
+        {
+            return View();
+        }
+
+        public IActionResult HuiMoeny()
+        {
+            return View();
+        }
+
+        public IActionResult FengK()
+        {
+            return View();
+        }
+
+
+        public IActionResult UserLogin()
+        {
+            return View();
+        }
+
+
+    }
+}
