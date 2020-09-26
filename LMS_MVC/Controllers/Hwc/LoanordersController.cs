68ed0bb54c8a5d19e64a57a85d9e3f3b98ffe634
韩文春 <@DESKTOP-HRSQVH5>
2020-09-26 14:01:42 +0800

    111

---
+++ LMS_MVC/Controllers/Hwc/LoanordersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMS_MVC.Controllers.Hwc
+{
+    public class LoanordersController : Controller
+    {
+        public IActionResult GetDing()
+        {
+            return View();
+        }
+        public IActionResult GetLonOrders()
+        {
+            return View();
+        }
+        public IActionResult GetBillList()
+        {
+            return View();
+        }
+        public IActionResult FTDing(int id)
+        {
+            ViewBag.id = id;
+            return View();
+        }
+        public IActionResult FTFang(int id)
+        {
+            ViewBag.id1 = id;
+            return View();
+        }
+    }
+}
