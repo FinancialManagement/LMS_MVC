ef36a3d277d253b2403f993156d9179911a6c5b4
忆梦小骚年 <@CXY>
2020-09-28 15:23:24 +0800

    mn

---
+++ LMS_MVC/Controllers/Cxy/SystemController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMS_MVC.Controllers
+{
+    public class SystemController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult GetUser()
+        {
+            return View();
+        }
+        public IActionResult LMS_LayUI(string name)
+        {
+            ViewBag.name = name;
+            return View();
+        }
+        public IActionResult UptUser(int id)
+        {
+            ViewBag.id = id;
+            return View();
+        }
+
+        public IActionResult LMS_UI()
+        {
+            
+            return View();
+        }
+    }
+}
